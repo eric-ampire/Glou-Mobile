f2098b3387c1985e3971a5a237387abd70267fcf
Japhet Majende <majendewangoy@gmail.com>
2018-10-02 13:45:04 +0200

    Add existing files

--- app/src/main/java/com/gmc/gloumobile/MainActivity.java
+++ app/src/main/java/com/gmc/gloumobile/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(android.R.layout.activity_list_item);
     }
 }
