4fe9268e4eecd9eebe9958f895dbc2c14fb7248e
Mario Majila <mariomajila6@gmail.com>
2018-10-03 16:53:53 +0100

    ADD Activite de SIGN UP

---
+++ app/src/main/java/com/gmc/gloumobile/SignUp.java
@@ -0,0 +1,14 @@
+package com.gmc.gloumobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
