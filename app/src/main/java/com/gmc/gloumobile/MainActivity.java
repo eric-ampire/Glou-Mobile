fe5563ccb857d0b5ccd9db1fd91fd40609b17874
Japhet Majende <majendewangoy@gmail.com>
2018-10-03 15:31:55 +0200

    UPDATE of the MainActivity.java files

--- app/src/main/java/com/gmc/gloumobile/MainActivity.java
+++ app/src/main/java/com/gmc/gloumobile/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -31,11 +33,70 @@ import java.util.Map;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends FragmentActivity implements OnMapReadyCallback,
+                GoogleApiClient.ConnectionCallbacks,
+                GoogleApiClient.OnConnectionFailedListener,
+                LocationListener{
+
+    private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    Marker mCurrLocationMarker;
+    //LocationRequest mLocationRequest;
+        int PROXIMITY_RADIUS = 1000;
+        double location, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(android.R.layout.activity_list_item);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+           //checkLocationPermission();
+        }
+        //On verifie les services de google play si ils sont disponiples
+        if(!checkGooglePlayService())
+        {
+            Log.d("on_Create", "Finishing test case since Google play" +
+                            " Service are not available");
+            finish();
+        }
+        else
+        {
+            Log.d("onCreate" , "Google Play Service available");
+        }
+        /*obtention du SupportMapFragment et des notifications
+        quand le map est pret à etre utiliser*/
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+                mapFragment.getMapAsync(this);
+
+
+    }
+    
+    private String getUrl(double latitude , double longitude, String
+            nearbyPlace)
+    {
+        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/findplacefromtext/");
+        googlePlaceUrl.append("location" +latitude+","+longitude);
+        googlePlaceUrl.append("&radius="+PROXIMITY_RADIUS);
+        googlePlaceUrl.append("&type="+nearbyPlace);
+        googlePlaceUrl.append("&sensor=true");
+        googlePlaceUrl.append("&key="+"AIzaSyCJ_yrrdce-QU26hS8H5FdelTrzOxFd4JA");
+
+        return  googlePlaceUrl.toString();
+    }
+    public void onConnected(Bundle bundle)
+    {
+        /*mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED)
+        {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }*/
     }
 }
